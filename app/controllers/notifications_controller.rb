413470d73c57ebb1b7e11c3a7019397ea73a3bd8
Amol <rails@kunalinfotech.net>
2011-09-19 15:32:59 +0530

    changes

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,5 +1,9 @@
 class NotificationsController < ApplicationController
   
+  def index
+    @notifications = Notification.find(:all, :order => 'id desc').paginate(:per_page => 6, :page => params[:page])
+  end
+  
   def show
     @notification = Notification.find(params[:id])
   end
--- app/controllers/tips_controller.rb
+++ app/controllers/tips_controller.rb
@@ -1,7 +1,7 @@
 class TipsController < ApplicationController
   
   def index
-    @tips = Tip.find(:all).paginate(:per_page => 6, :page => params[:page])
+    @tips = Tip.find(:all, :order => 'id desc').paginate(:per_page => 6, :page => params[:page])
   end
   
   def show
