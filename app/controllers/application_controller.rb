cc7456cc4fb759461ff039f69bce282d2c4548a1
Amol <rails@kunalinfotech.net>
2011-09-22 11:20:21 +0530

    changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,13 @@ class ApplicationController < ActionController::Base
   def after_sign_out_path_for(resource_or_scope)
       new_admin_session_path
   end
+  
+  def rescue_with_handler(exception)
+      redirect_to '/'
+    end
+
+    def method_missing(id, *args)
+      redirect_to '/'
+    end
+  
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
   
   def show
-    @page = Page.find(params[:id])
+    @page = Page.find(params[:id]) 
     
     @title = "#{@page.title}"
   end
