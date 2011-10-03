16dbbad05825dab604260a932f1aefe2c38ac5d3
Amol <rails@kunalinfotech.net>
2011-10-03 12:49:05 +0530

    changes

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,6 +1,6 @@
 class Notification < ActiveRecord::Base
   validates_presence_of :title, :description
-  validates_length_of :title, :within => 2..100
+  validates_length_of :title, :within => 2..200
   
   has_friendly_id :title, :use_slug => true
   
