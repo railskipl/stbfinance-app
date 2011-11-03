c762a65addec7eedfb4f97c92e96380f4e2db532
Amol <rails@kunalinfotech.net>
2011-11-03 18:06:54 +0530

    changes

--- app/models/banner.rb
+++ app/models/banner.rb
@@ -3,7 +3,8 @@ class Banner < ActiveRecord::Base
   has_attached_file :photo, 
                     :styles => { :thumb => "230x200" },
                     
-                   
+                    :storage => :s3, :s3_credentials => "#{RAILS_ROOT}/config/s3.yml",
+                    :path => "public/attachments/ads/:id/:style/:basename.:extension",
                     
                     :convert_options => {
                           :thumb => "-background '#F7F4F4' -compose Copy -gravity center -extent 230x200"
